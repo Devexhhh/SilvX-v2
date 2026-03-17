6f17a735501048abdf53329cfc81ac8324bc8c34
Devexhhh <kumarsujal.codes@gmail.com>
2026-03-17 19:16:17 +0530

    added silver routes

--- apps/api/src/routes/silver.ts
+++ apps/api/src/routes/silver.ts
@@ -3,4 +3,20 @@ import { buySilver } from "../services/silver.service"
 
 export const silverRouter = Router()
 
-silverRouter.post("/buy", buySilver)
+silverRouter.post("/buy", async (req, res) => {
+
+    const { userId, amount } = req.body
+
+    const idempotencyKey = `manual-${Date.now()}-${userId}`
+
+    const grams = await buySilver(
+        userId,
+        amount,
+        idempotencyKey
+    )
+
+    res.json({
+        success: true,
+        grams
+    })
+})
