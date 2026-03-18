fc6cd119b39ae97b55a95d2eb4c93acb57893674
Devexhhh <kumarsujal.codes@gmail.com>
2026-03-18 18:23:02 +0530

    addded jwt

---
+++ apps/api/src/lib/jwt.ts
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken"
+
+const JWT_SECRET = process.env.JWT_SECRET || "dev_secret"
+
+export const signToken = (userId: string) => {
+
+    return jwt.sign(
+        { userId },
+        JWT_SECRET,
+        { expiresIn: "7d" }
+    )
+
+}
+
+export const verifyToken = (token: string) => {
+
+    return jwt.verify(token, JWT_SECRET) as { userId: string }
+
+}
